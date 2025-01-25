f1367c0ff888adfdbcae126c06b4781f57ec051a
Raquel <raquelpm23@gmail.com>
2025-01-25 19:54:08 +0100

    Wip canon, ready for first publish

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
 
+import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
+
 export default defineConfig({
   // used to generate images
   site: 'https://raquelfishes.github.io',
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [sitemap(), UnoCSS({ injectReset: true }), tailwind(), icon()],
   vite: {
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
