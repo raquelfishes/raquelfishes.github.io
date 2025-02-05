8122ec308bed780171ded3ffaa263fae866d7431
raquelpeces <raquel.peces@unity3d.com>
2025-02-05 16:33:09 +0100

    WIP canon

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
+import { text } from 'd3';
 
 const blogCollection = defineCollection({
   loader: glob({ pattern: '**/[^_]*.md', base: "./src/data/blog" }),
@@ -17,16 +18,31 @@ const blogCollection = defineCollection({
 
 const nowCollection = defineCollection({
     loader: glob({ pattern: '**/[^_]*.md', base: "./src/data/now" }),
-    schema: ({ image }) =>
-      z.object({
+    schema: z.object({
         title: z.string(),
         author: z.string(),
         pubDate: z.string().transform((str) => new Date(str)),
         draft: z.boolean().optional().default(false),
-      }),
-  });
+     }),
+});
+
+const canonCollection = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.json', base: "./src/data/canon" }),
+    schema: z.object({
+        title: z.string(),
+        items: z.array(
+            z.object({
+                title: z.string(),
+                type: z.string(),
+                text: z.string(),
+                quote: z.string().optional(),
+                link: z.string().url().optional(),
+        })),
+    }),
+});
 
 export const collections = {
   blog: blogCollection,
   now: nowCollection,
+  canon: canonCollection,
 };
