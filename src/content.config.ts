332603b265add790fd7fb48e76b2d2738030ddd0
raquelpeces <raquel.peces@unity3d.com>
2025-02-21 20:22:09 +0100

    Blog skeleton ready

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,20 +2,6 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { text } from 'd3';
 
-const blogCollection = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.md', base: "./src/data/blog" }),
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      author: z.string(),
-      tags: z.array(z.string()),
-      description: z.string(),
-      pubDate: z.string().transform((str) => new Date(str)),
-      imgUrl: image(),
-      draft: z.boolean().optional().default(false),
-    }),
-});
-
 const nowCollection = defineCollection({
     loader: glob({ pattern: '**/[^_]*.md', base: "./src/data/now" }),
     schema: z.object({
@@ -43,8 +29,8 @@ const canonCollection = defineCollection({
     }),
 });
 
-const articleCollection = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/data/articles" }),
+const blogCollection = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.md', base: "./src/data/blog" }),
     schema: z.object({
         title: z.string(), // Title of the article (required)
         description: z.string(), // Description of the article (required)
@@ -71,5 +57,4 @@ export const collections = {
   blog: blogCollection,
   now: nowCollection,
   canon: canonCollection,
-  articles: articleCollection,
 };
