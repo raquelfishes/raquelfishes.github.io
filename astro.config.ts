b162741a6680f4211f8411f35c2ac53af206d75f
Raquel <raquelpm23@gmail.com>
2025-02-22 11:16:55 +0100

    Icon for dark/light mode

--- astro.config.ts
+++ astro.config.ts
@@ -6,12 +6,13 @@ import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
 import partytown from '@astrojs/partytown';
+import preact from '@astrojs/preact';
 
 export default defineConfig({
   // used to generate images
   site: 'https://raquelfishes.github.io',
   trailingSlash: 'ignore',
-  integrations: [sitemap(), tailwind(), icon(), partytown()],
+  integrations: [sitemap(), tailwind(), icon(), partytown(), preact()],
   vite: {
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
